88674636da6b64d20c1ed01c33d7453dbc76b5b0
Rodrigo Vasco <r.vasco@hotmail.com>
2018-06-06 16:16:53 -0300

    FeCriação da Entidade Usuario

---
+++ src/Entity/Usuario.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UsuarioRepository")
+ * @UniqueEntity("email", message="Esse e-mail já está em uso")
+ */
+class Usuario
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     * @Assert\NotBlank(message="Campo nome não pode ser vazio!")
+     */
+    private $nome;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     * @Assert\NotBlank(message="Campo email não pode ser vazio!")
+     * @Assert\Email(message="Informe um e-mail válido!")
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     * @Assert\NotBlank(message="Informe sua senha!")
+     */
+    private $senha;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="string", length=32)
+     */
+    private $token;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     * @Gedmo\Timestampable(on="create")
+     *
+     */
+    private $data_cadastro;
+
+    /**
+     * @ORM\Column(type="datetime_immutable", nullable=true)
+     * @Gedmo\Timestampable(on="update")
+     *
+     */
+    private $data_alteracao;
+
+    public function __construct ()
+    {
+        $this->status = false;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getNome(): ?string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getSenha(): ?string
+    {
+        return $this->senha;
+    }
+
+    public function setSenha(string $senha): self
+    {
+        $this->senha = $senha;
+
+        return $this;
+    }
+
+    public function getStatus(): ?bool
+    {
+        return $this->status;
+    }
+
+    public function setStatus(bool $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getToken(): ?string
+    {
+        return $this->token;
+    }
+
+    public function setToken(string $token): self
+    {
+        $this->token = $token;
+
+        return $this;
+    }
+
+    public function getDataCadastro(): ?\DateTimeImmutable
+    {
+        return $this->data_cadastro;
+    }
+
+    public function setDataCadastro(\DateTimeImmutable $data_cadastro): self
+    {
+        $this->data_cadastro = $data_cadastro;
+
+        return $this;
+    }
+
+    public function getDataAlteracao(): ?\DateTimeImmutable
+    {
+        return $this->data_alteracao;
+    }
+
+    public function setDataAlteracao(?\DateTimeImmutable $data_alteracao): self
+    {
+        $this->data_alteracao = $data_alteracao;
+
+        return $this;
+    }
+}
