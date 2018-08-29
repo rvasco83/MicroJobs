3e9c23d6c64e21bcc2c0b2797062dd8d0da97f56
Rodrigo Vasco <r.vasco@hotmail.com>
2018-08-29 16:49:45 -0300

    Filtrando jobs por status

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+
 use App\Entity\Servico;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -31,14 +32,18 @@ class DefaultController extends Controller
     /**
      * @Route("/painel", name="painel")
      * @Template("default/painel.html.twig")
+     * @param UserInterface $user
+     * @return array
      */
-    public function painel(UserInterface $user)
+    public function painel(UserInterface $user, Request $request)
     {
+        $status = $request->get("busca_filtro");
         $micro_jobs = $this->em->getRepository(Servico::class)
-            ->findByUsuarioAndStatus($user);
+            ->findByUsuarioAndStatus($user, $status);
 
         return [
-            'micro_jobs' => $micro_jobs
+            'micro_jobs' => $micro_jobs,
+            'status' => $status
         ];
     }
 }
